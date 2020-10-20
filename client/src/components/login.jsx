613109b9262b552aa8362d9bb38b1e9b2013c1e7
Steven Contreras <SFContra@gmail.com>
2020-10-19 22:32:56 -0700

    Set inputs to be stateful

--- client/src/components/login.jsx
+++ client/src/components/login.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Login({ handleLogin, user, setUser }) {
 
   function handleChange(e) {
-    console.log(e.target)
+    console.log(e.target.value)
+    const entry = e.target.value
+    const name = e.target.name
+    setUser({
+      ...user,
+      [name]: entry
+    });
   }
 
   return (
@@ -28,11 +34,11 @@ function Login({ handleLogin, user, setUser }) {
         </label>
         <button type="button" onClick={() => handleLogin("1")} >Login</button>
       </form>
-      <div>
-        <a>Forgot password?</a>      
-      </div>
       <div>Not a member?
         <a type="button" onClick={() => handleLogin("2")}> Sign up!</a>      
+      </div>
+      <div>
+        <a>Forgot password?</a>      
       </div>
     </main>
   )
--- client/src/components/signUp.jsx
+++ client/src/components/signUp.jsx
@@ -1,19 +1,40 @@
 import React from "react";
 
-function SignUp() {
+function SignUp({ handleLogin, user, setUser }) {
+  
+  function handleChange(e) {
+    console.log(e.target.value)
+    const entry = e.target.value
+    const name = e.target.name
+    setUser({
+      ...user,
+      [name]: entry
+    });
+  }
 
   return (
     <main>
       <h4>Your Account Information</h4>
       <form>
-        <label>Email Address:</label>
-        <input></input>
+        <label>Email:
+          <input
+            name="email"
+            type="email"
+            value={user.email}
+            onChange={handleChange}>
+          </input>
+        </label>
         <label>Password:</label>
-        <input></input>
+        <input
+          name="password"
+          type="password"
+          value={user.password}
+          onChange={handleChange}>
+        </input>
         <button>Continue</button>
       </form>
       <div>Already a member?
-        <a> Log In</a>      
+        <a type="button" onClick={() => handleLogin("0")}> Login</a>      
       </div>
       <div>
         <a>Forgot password?</a>      
